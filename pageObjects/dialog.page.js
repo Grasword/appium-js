d2a4f650152c14274bc92ef6f33dd25fc007b809
Oleg Zhmaev <grasword6@gmail.com>
2022-06-13 17:17:33 +0300

    created dialog `pageObect`

---
+++ pageObjects/dialog.page.js
@@ -0,0 +1,53 @@
+class Dialog {
+  get appBtn() {
+    return $('~App')
+  }
+  get alertDialogBtn() {
+    return $('//android.widget.TextView[@content-desc="Alert Dialogs"]')
+  }
+  get textEntryDialogBtn() {
+    return $('//android.widget.Button[@content-desc="Text Entry dialog"]')
+  }
+  get userNameField() {
+    return $('//android.widget.EditText[@resource-id="io.appium.android.apis:id/username_edit"]')
+  }
+  get passwordField() {
+    return $('//android.widget.EditText[@resource-id="io.appium.android.apis:id/password_edit"]')
+  }
+
+  get dialogOkBtn() {
+    return $('//android.widget.Button[@resource-id="android:id/button1"]')
+  }
+  get dialogCancelBtn() {
+    return $('//android.widget.Button[@resource-id="android:id/button2"]')
+  }
+
+  get viewBtn() {
+    return $('//android.widget.TextView[@content-desc="Views"]')
+  }
+  get tabsBtn() {
+    return $('//android.widget.TextView[@content-desc="Tabs"]')
+  }
+  get contentByIdBtn() {
+    return $('//android.widget.TextView[@content-desc="1. Content By Id"]')
+  }
+
+  get tabs() {
+    return $$('android.widget.LinearLayout')
+  }
+  get tab1Details() {
+    return $('~tab1')
+  }
+  get tab2Details() {
+    return $('~tab2')
+  }
+  get tab3Details() {
+    return $('~tab3')
+  }
+
+  get repeatAlarmBtn() {
+    return $('//android.widget.Button[@content-desc="Repeat alarm"]')
+  }
+}
+
+module.exports = new Dialog()
